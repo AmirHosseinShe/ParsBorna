c9e5dbe17e3b70dd3b61a874bc76bab3f125d3cf
FarhanFDK <Farhanabdollahisd@gmail.com>
2020-11-13 14:18:14 +0330

    CHANGED

---
+++ index.php
@@ -0,0 +1,145 @@
+<!doctype html>
+<html>
+    <head>
+    <meta charset="utf-8">
+    <title> Pars Borna Project</title>
+    <link rel="stylesheet" type="text/css" href="src/css/index.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="shortcut icon" href="favicon.ico"/>
+    <script src="https://kit.fontawesome.com/b39b75221a.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <!-- تبلیغات -->
+
+    <div class="advert">
+        <h1 class="h1">درج تبلیغات شما</h1>
+    </div>
+
+    <!-- تبلیغات -->
+
+    <!-- هدر سایت -->
+
+    <header>
+        <nav>
+            <ul class="navbar">
+            <li><a href="#">درباره ما</a></li>
+            <li><a href="#">گالری</a></li>
+            <li><a href="#">ثبت سفارش</a></li>
+            <li><a href="#">صفحه اصلی</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <!-- هدر سایت -->
+
+    <!-- مرکز سایت -->
+
+    <div class="slider">
+        <div class="section">
+            <h2>تــــــازه هــــا</h2>
+            <div class="image-carousel style2" >
+                <div class="flex-viewport" style="overflow: hidden; position: relative;">
+                    <ul class="slides image-box hotel listing-style1" style="width: 100%; transition-duration: 0.6s; transform: translate3d(-300px, 0px, 0px);">  
+                        <li style="margin-left:10px;  width: 350px; float: right; display: block;">
+                            <article class="box">
+                                <figure> <a href="#" class="hover-effect popup-gallery"><img width="270" height="160" alt="" src="#" ></a> </figure>
+                                <div class="details"> <span class="price"><small>html/css</small> تومان50000</span>
+                                    <h4 class="box-title">اســلایــدر<small>Slide Bar</small></h4>
+                                    <div class="feedback">
+                                        <div data-placement="bottom" data-toggle="tooltip" class="fa fa-star" title="" data-original-title="4 stars"><span style="width: 80%;" class="five-stars"></span></div> <span class="review">485 reviews</span>
+                                    </div>
+                                    <p class="description">اسلاید بار ها</p>
+                                    <div class="action"> <a class="button btn-small" href="#">دسته بندی</a> <a class="button btn-small yellow popup-map" href="#" data-box="40.463667, -3.749220">ادامه و خرید</a> </div>
+                                </div>
+                            </article>
+                        </li>
+
+                        <li style="width: 350px; float: right; display: block;">
+                            <article class="box">
+                                <figure> <a href="#" class="hover-effect popup-gallery"><img width="270" height="160" alt="" src="#" draggable="false"></a> </figure>
+                                <div class="details"> <span class="price"> <small>html/css/js</small> تومان50000 </span>
+                                    <h4 class="box-title">مـــنــو<small>Nav Bar</small></h4>
+                                    <div class="feedback">
+                                        <div data-placement="bottom" data-toggle="tooltip" class="fa fa-star" title="" data-original-title="4 stars"><span style="width: 80%;" class="five-stars"></span></div> <span class="review">270 reviews</span>
+                                    </div>
+                                    <p class="description">مـــنــو بوت استــرپت</p>
+                                    <div class="action"> <a class="button btn-small" href="#">دسته بندی</a> <a class="button btn-small yellow popup-map" href="#" data-box="48.856614, 2.352222">ادامه و خرید</a> </div>
+                                </div>
+                            </article>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="sidebar"></div>
+    <div class="storebar"></div>
+
+    <!-- مرکز سایت -->
+
+    <!-- مرکز سایت 2 -->
+        <!-- content start -->
+    <div class="container" style="margin-top: 10px;">
+        <div class="jumbotron">
+            <section class="hero">
+                <div class="waves">
+                    <p style="text-align: center; margin-top: 70px;">سهولت انتخاب ، خلاقیت در طراحی ، رزومه قوی</p>
+                </div>
+            </section>
+        </div>
+        <div class="row flex">
+            <div class="col-lg-4 col-sm-6">
+                <div class="thumbnail">
+                    <img src="#">
+                </div>
+            </div>
+            <div class="col-lg-4 col-sm-6">
+                <div class="thumbnail">
+                    <img src="#">
+                </div>
+            </div>
+            <div class="col-lg-4 col-sm-6">
+                <div class="thumbnail">
+                    <img src="#">
+                </div>
+            </div>
+            <div class="col-lg-4 col-sm-6">
+                <div class="thumbnail">
+                    <img src="#">
+                </div>
+            </div>
+            <div class="col-lg-4 col-sm-6">
+                <div class="thumbnail">
+                    <img src="#">
+                </div>
+            </div>
+            <div class="col-lg-4 col-sm-6">
+                <div class="thumbnail">
+                    <img src="#">
+                </div>
+            </div>
+            <div class="col-lg-4 col-sm-6">
+                <div class="thumbnail">
+                    <img src="#">
+                </div>
+            </div>
+            <div class="col-lg-4 col-sm-6">
+                <div class="thumbnail">
+                    <img src="#">
+                </div>
+            </div>
+            <div class="col-lg-4 col-sm-6">
+                <div class="thumbnail">
+                    <img src="#">
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.2.1.js" integrity="sha256-DZAnKJ/6XZ9si04Hgrsxu/8s717jcIzLy3oi35EouyE=" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <!-- مرکز سایت 2 -->
+
+</body>
+</html>
